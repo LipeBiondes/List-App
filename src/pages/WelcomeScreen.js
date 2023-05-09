bc707033f6c18743ea8b9830a488c61370dc63a8
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2023-05-08 21:13:45 -0300

    WelcomScreen-Routes

--- src/pages/WelcomeScreen.js
+++ src/pages/WelcomeScreen.js
@@ -1,14 +1,24 @@
 import * as React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+import Icon from 'react-native-vector-icons/FontAwesome'
 const WelcomeScreen = ({ navigation }) => {
   return (
-    <View>
+    <View style={style.container}>
       <TouchableOpacity onPress={() => navigation.navigate('Main')}>
         <Text> Vamos Começar!</Text>
+        <Icon name="fa-tasks" size={35} color="#fff" />
       </TouchableOpacity>
     </View>
   )
 }
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 5,
+    paddingTop: Constants.statusBarHeight + 10,
+    backgroundColor: '#f0ee87'
+  }
+})
 export default WelcomeScreen
